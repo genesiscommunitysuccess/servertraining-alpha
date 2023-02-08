0f8ee023c3f2fa6b93e7bafd4ea3d371b1ea033e
dbarros1979 <daniel.barros@genesis.global>
2023-02-08 19:39:23 +0000

    Fixing exercise 5.2

--- server/jvm/alpha-config/src/main/resources/cfg/alpha-fields-dictionary.kts
+++ server/jvm/alpha-config/src/main/resources/cfg/alpha-fields-dictionary.kts
@@ -11,10 +11,10 @@
 
 fields {
 
-    field("TRADE_ID", type = STRING)
+    field("TRADE_ID", type = STRING, maxSize = 100)
     field("QUANTITY", type = INT, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS)
     field("PRICE", type = DOUBLE)
-    field("SYMBOL", type = STRING, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS)
+    field("SYMBOL", type = STRING, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS, maxSize = 50)
     field("DIRECTION", type = ENUM("BUY", "SELL", default = "BUY"))
 
     field("COUNTERPARTY_ID", type = STRING)
@@ -30,7 +30,7 @@ fields {
     field("ASSET_CLASS", type = STRING)
 
     field("TRADE_DATE", type = DATE, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS)
-    field("ENTERED_BY", type = STRING, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS)
+    field("ENTERED_BY", type = STRING, nullable = SysDef.NULLABILITY_FOR_TRADE_FIELDS, maxSize = 100)
     field(name = "TRADE_STATUS", type = ENUM("NEW", "ALLOCATED", "CANCELLED", default = "NEW"))
 
     field("POSITION_ID", type = STRING)
@@ -39,8 +39,8 @@ fields {
     field("VALUE", type = DOUBLE)
     field("PNL", type = DOUBLE)
 
-    field("FAVOURITE_TRADE_ID", type = STRING)
-    field("FAVOURITE_TRADE_USER_NAME", type = STRING)
+    field("FAVOURITE_TRADE_ID", type = STRING, maxSize = 100)
+    field("FAVOURITE_TRADE_USER_NAME", type = STRING, maxSize = 100)
 
     field("BEEN_AUDITED", type = BOOLEAN)
 }
