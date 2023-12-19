7b7dde09b156bb6c80d996bf8ff6310c246de0c1
dbarros1979 <daniel.barros@genesis.global>
2023-12-19 09:29:08 +0000

    Adjusting GENESIS_COMPACT_PROCESSES

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-streamer-client.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-streamer-client.kts
@@ -2,7 +2,7 @@ streamerClients {
     streamerClient(clientName = "TRADE_AUDIT_RESPONSE") {
         dataSource(processName = "ALPHA_STREAMER", sourceName = "TRADE_AUDIT_STREAM")
         onMessage {
-            send("ALPHA_EVENT_HANDLER", "EVENT_TRADE_AUDIT_STREAM")
+            send("GENESIS_COMPACT_PROCESS", "EVENT_TRADE_AUDIT_STREAM")
         }
     }
 }
