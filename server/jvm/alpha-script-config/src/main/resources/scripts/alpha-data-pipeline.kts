b9abefb86052d94cd782014ad5115d3fa9690b40
GuiWar <120386099+GuiWar@users.noreply.github.com>
2024-03-06 13:52:58 +0000

    Update alpha-data-pipeline.kts

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-data-pipeline.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-data-pipeline.kts
@@ -1,18 +1,26 @@
+import global.genesis.commons.standards.GenesisPaths
+import global.genesis.data.ingest.source.SinkOperations
+import global.genesis.data.ingest.transform.SinkOperation
 import global.genesis.gen.config.tables.COUNTERPARTY
+import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_ID
 import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_NAME
 import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_LEI
 
-pipelines {
-  csvSource("cp-pipeline") {
-    location = "file:/home/alpha/run/runtime/fileIngress?fileName=cp-pipeline.csv"
+pipelines{
+    csvSource("cp-pipeline"){
+        location = "file:${GenesisPaths.runtime()}/fileIngress?fileName=cp-pipeline.csv&delete=true"
+        errorStrategy = AllOrNothing()
 
-    map("mapper-name", COUNTERPARTY) {
-      COUNTERPARTY_NAME {
-        property = "COUNTERPARTY_NAME"
-      }
-      COUNTERPARTY_LEI {
-        property = "COUNTERPARTY_LEI"
-      }
+        map("mapper-name", COUNTERPARTY){
+            COUNTERPARTY_ID{
+                property = "Id"
+            }
+            COUNTERPARTY_NAME{
+                property = "Counterparty Name"
+            }
+            COUNTERPARTY_LEI{
+                property = "Counterparty Lei"
+            }
+        }
     }
-  }
 }
