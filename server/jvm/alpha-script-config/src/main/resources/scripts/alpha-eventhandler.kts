e8e42493968debf2eba1f1eb828d4daacf82e25b
GuiWar <120386099+GuiWar@users.noreply.github.com>
2024-03-06 13:53:34 +0000

    Update alpha-eventhandler.kts

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-eventhandler.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-eventhandler.kts
@@ -5,11 +5,15 @@ import global.genesis.gen.dao.Trade
 import global.genesis.alpha.message.event.TradeAllocated
 import global.genesis.alpha.message.event.TradeCancelled
 import global.genesis.alpha.message.event.PositionReport
-import global.genesis.alpha.message.event.TradeStandardization
-import global.genesis.alpha.message.event.CustomInstrumentEventReply
 import global.genesis.commons.standards.GenesisPaths
-import global.genesis.gen.view.repository.TradeViewAsyncRepository
+import global.genesis.db.rx.entity.multi.AsyncEntityDb
 import global.genesis.jackson.core.GenesisJacksonMapper
+import global.genesis.alpha.eventhandler.validate.*
+import global.genesis.alpha.eventhandler.commit.*
+import global.genesis.alpha.message.event.*
+import global.genesis.clustersupport.service.ServiceDiscovery
+import global.genesis.message.core.event.ApprovalType
+
 
 /**
  * System              : Genesis Business Library
@@ -21,45 +25,41 @@ import global.genesis.jackson.core.GenesisJacksonMapper
  *
  * Modification History
  */
-val tradeViewRepo = inject<TradeViewAsyncRepository>()
+val tradeViewRepo = inject<AsyncEntityDb>()
 
 eventHandler {
     val stateMachine = inject<TradeStateMachine>()
 
-    eventHandler<Trade>(name = "TRADE_INSERT") {
+    eventHandler<Trade, CustomTradeEventReply>(name = "TRADE_INSERT") {
         schemaValidation = false
-        permissionCodes = listOf("INSERT_TRADE")
-        onValidate { event ->
-            val message = event.details
-            verify {
-                entityDb hasEntry Counterparty.byId(message.counterpartyId.toString())
-                entityDb hasEntry Instrument.byId(message.instrumentId.toString())
-            }
-            ack()
+
+        onException{_ , throwable ->
+            CustomTradeEventReply.TradeNack("ERROR: ${throwable.message}")
         }
+
+
+        onValidate {event ->
+            ValidateTrade.validateInsert(event, entityDb)
+            LOG.info(event.userName)
+            CustomTradeEventReply.ValidationTradeAck()
+        }
+
         onCommit { event ->
             val trade = event.details
 
             if (trade.quantity!! > 0) {
                 trade.enteredBy = event.userName
                 stateMachine.insert(entityDb, trade)
-                ack()
+                CustomTradeEventReply.TradeAck("Trade was successfully inserted: ${trade.tradeId}")
             }
             else {
-                nack("Quantity must be positive")
+                CustomTradeEventReply.TradeNack("Quantity must be greater than zero")
             }
         }
     }
 
     eventHandler<Trade>(name = "TRADE_MODIFY", transactional = true) {
-        onValidate { event ->
-            val message = event.details
-            verify {
-                entityDb hasEntry Counterparty.ById(message.counterpartyId.toString())
-                entityDb hasEntry Instrument.byId(message.instrumentId.toString())
-            }
-            ack()
-        }
+
         onCommit { event ->
             val trade = event.details
             stateMachine.modify(entityDb, trade)
@@ -67,20 +67,58 @@ eventHandler {
         }
     }
 
-    eventHandler<Counterparty>(name = "COUNTERPARTY_INSERT", transactional = true) {
-        onCommit { event ->
-            try {
-                entityDb.insert(event.details)
-                ack()
+    eventHandler<Trade>(name="TRADE_UPSERT"){
+        schemaValidation = false
+        onValidate {
+            ValidateTrade.validateUpsert(it,entityDb)
+            ack()
+        }
+        onCommit {
+            CommitTrade.upsert(it,entityDb)
+            ack()
+        }
+    }
+
+    eventHandler<Counterparty, CustomCounterpartyEventReply>(name="COUNTERPARTY_INSERT", transactional = true){
+        schemaValidation = false
+
+        onException{ _ , throwable ->
+            CustomCounterpartyEventReply.CounterpartyNack("ERROR: ${throwable.message}")
+        }
+        onValidate {
+            require(!it.details.counterpartyId.contains("1234")){
+                "COUNTERPARTY_ID cannot contain 1234"
             }
-            catch (e: Exception) {
-                val errorMessage = "COUNTERPARTY_INSERT -> Error on adding Counterparty. Error message: ${e.message}. Error cause: ${e.cause}"
-                nack(errorMessage)
-            }
+            CustomCounterpartyEventReply.ValidationCounterpartyAck()
+        }
+        onCommit {
+            entityDb.insert(it.details)
+            CustomCounterpartyEventReply.CounterpartyAck("Counterparty successfully inserted: ${it.details.counterpartyId}")
+        }
+    }
+
+    eventHandler<Counterparty>(name="COUNTERPARTY_UPSERT"){
+        schemaValidation = false
+
+        onCommit {
+            CommitCounterparty.upsert(it,entityDb)
+            ack()
+        }
+    }
+
+    eventHandler<Instrument>(name="INSTRUMENT_UPSERT"){
+        schemaValidation = false
+        onCommit {
+            CommitInstrument.upsert(it,entityDb)
+            ack()
         }
     }
 
     eventHandler<Counterparty>(name = "COUNTERPARTY_DELETE", transactional = true) {
+        onValidate{
+            ValidateCounterparty.validateDelete(it, entityDb)
+            ack()
+        }
         onCommit { event ->
             entityDb.delete(event.details)
             ack()
@@ -94,18 +132,63 @@ eventHandler {
         }
     }
 
-    eventHandler<Instrument, CustomInstrumentEventReply>(name = "INSTRUMENT_INSERT", transactional = true) {
-        onException { event, throwable ->
-            CustomInstrumentEventReply.InstrumentEventNack(throwable.message!!)
+
+    eventHandler<Instrument, CustomInstrumentEventReply>(name="INSTRUMENT_INSERT", transactional = true){
+        schemaValidation = false
+
+        onException{ _ , throwable ->
+            CustomInstrumentEventReply.InstrumentNack("ERROR: ${throwable.message}")
         }
+
         onValidate {
-            val instrumentEvent = it.details
-            require(instrumentEvent.instrumentName!!.length > 2) { "instrumentName needs at least 3 characters" }
-            CustomInstrumentEventReply.InstrumentEventValidateAck()
+            require(!it.details.instrumentId.contains("1234")){
+                "INSTRUMENT_ID cannot contain 1234"
+            }
+            CustomInstrumentEventReply.ValidationInstrumentAck()
         }
-        onCommit { event ->
-            val result = entityDb.insert(event.details)
-            CustomInstrumentEventReply.InstrumentEventAck(result.record.instrumentId)
+        onCommit {
+            entityDb.insert(it.details)
+            CustomInstrumentEventReply.InstrumentAck("Instrument successfully inserted: ${it.details.instrumentId}")
+        }
+    }
+
+    contextEventHandler<Instrument, List<Trade>>(name = "INSTRUMENT_DELETE_CASCADE", transactional = true){
+        onValidate{ event ->
+            val trades = ValidateInstrument.validateCascadeDelete(event, entityDb)
+            validationResult(ack(),trades)
+        }
+
+        onCommit{ event, trades ->
+            if(trades != null){
+                for (trade in trades){
+                    LOG.info("Deleting trade ${trade.tradeId}")
+                    entityDb.delete(Trade.byId(trade.tradeId))
+                }
+            }
+            entityDb.delete(event.details)
+            ack()
+        }
+    }
+
+    contextEventHandler<Counterparty, List<Trade>>(name = "COUNTERPARTY_DELETE_CASCADE", transactional = true){
+        onException{ _ , throwable ->
+            nack("ERROR: ${throwable.message}")
+        }
+
+        onValidate{ event ->
+            val trades = ValidateCounterparty.validateCascadeDelete(event, entityDb)
+            validationResult(ack(),trades)
+        }
+
+        onCommit{ event, trades ->
+            if(trades != null){
+                for (trade in trades){
+                    LOG.info("Deleting trade ${trade.tradeId}")
+                    entityDb.delete(Trade.byId(trade.tradeId))
+                }
+            }
+            entityDb.delete(event.details)
+            ack()
         }
     }
 
@@ -151,9 +234,9 @@ eventHandler {
             val positionReportFolder = File(GenesisPaths.runtime()).resolve("position-minute-report")
             if (!positionReportFolder.exists()) positionReportFolder.mkdirs()
 
-            tradeViewRepo.getBulk()
+            tradeViewRepo.getBulk(TRADE)
                 .toList()
-                .groupBy { it.counterpartyName }
+                .groupBy { Trade.COUNTERPARTY_ID }
                 .forEach { (counterParty, trades) ->
                     val file = positionReportFolder.resolve("${counterParty}_$today.csv")
                     if (file.exists()) file.delete()
@@ -163,29 +246,74 @@ eventHandler {
             ack()
         }
     }
+    eventHandler<Company> (name = "COMPANY_INSERT", transactional = true){
+        onException{ _ , throwable ->
+            nack("ERROR: ${throwable.message}")
+        }
 
-    eventHandler<TradeStandardization>(transactional = true) {
-        onCommit {
-            val tradesNegativePrices = entityDb.
-                getBulk(TRADE).toList()
-                .filter { it.price < 0 }
+        requiresPendingApproval { event ->
+            event.approvalMessage = "My custom approval message"
+            event.userName != "system.user"
+        }
 
-            tradesNegativePrices.forEach { t ->
-                t.price = 0.0
+        onValidate {
+            val company = it.details
+
+            val pendingApprovals = entityDb.getBulk(APPROVAL)
+                .filter { it.approvalStatus == ApprovalStatus.PENDING }.toList()
+
+
+            pendingApprovals.forEach {
+                val companyId = it.eventMessage
+                    .split("\",\"")
+                    .find{ it.contains("COMPANY_ID")}
+                    ?.split(":")
+                    ?.get(2)
+                    ?.removePrefix("\"")
+                LOG.info(companyId)
+                if (companyId == company.companyId){
+                    throw NoSuchElementException ("Company insert for ${company.companyId} has already been sent for approval, choose another company ID")
+                }
             }
 
-            entityDb.modifyAll(*tradesNegativePrices.toList().toTypedArray())
+
+            approvableAck(
+                entityDetails = listOf(
+                    ApprovalEntityDetails(
+                        entityTable = "COMPANY",
+                        entityId = company.companyId,
+                        approvalType = ApprovalType.NEW,
+                    )
+                ),
+                approvalMessage = "Company insert for ${company.companyId} has been sent for approval",
+                approvalType = ApprovalType.NEW,
+                additionalDetails = "Custom additional details"
+            )
+        }
+
+        onCommit {
+            val pendingApprovals = entityDb.getBulk(APPROVAL)
+            val company = it.details
+            entityDb.insert(company)
+            LOG.info("Company insert for ${company.companyId} successfully")
             ack()
-       }
+        }
     }
 
-    eventHandler<TradeAudit>(name = "TRADE_AUDIT_STREAM") {
-        onCommit { event ->
+    eventHandler<TradeAudit>(name = "TRADE_AUDIT_STREAM"){
+        schemaValidation = false
+        onCommit{ event ->
             val message = event.details
-            stateMachine.modify(entityDb, message.tradeId) { trade ->
-                trade.beenAudited = true
+            if(message.beenAudited == false || message.beenAudited == null){
+                stateMachine.modify(entityDb,message.tradeId){
+                    it.beenAudited = true
+                }
+                LOG.info("Trade_ID: ${message.tradeId} validated")
+            }else {
+                LOG.info("Trade_ID: ${message.tradeId} already validated")
             }
             ack()
         }
     }
 }
+
