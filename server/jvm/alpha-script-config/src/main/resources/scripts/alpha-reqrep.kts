128beea225c01389f9d737d9f0cdb154111dd7e8
GuiWar <120386099+GuiWar@users.noreply.github.com>
2024-03-06 13:53:51 +0000

    Update alpha-reqrep.kts

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-reqrep.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-reqrep.kts
@@ -9,8 +9,23 @@
  * Modification History
  */
 requestReplies {
-    requestReply("ALL_COUNTERPARTIES", COUNTERPARTY_VIEW) {
-        rowReturnLimit = 20
-        timeout = 10
+    requestReply("TRADE_DETAILS",TRADE){
+        request {
+            COUNTERPARTY_ID withTransformation { type, _ ->
+                if (type == "2") {
+                    "UNKOWN"
+                } else {
+                    type
+                }
+            }
+        }
+
+        reply {
+            TRADE_ID
+            COUNTERPARTY_ID
+            INSTRUMENT_ID
+            QUANTITY
+            PRICE
+        }
     }
 }
